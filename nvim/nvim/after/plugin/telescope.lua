3ea487a03eaa54d040974dfcb5a3e501966d8403
p swiercz <p.swiercz@tafira.studio>
2024-09-30 13:47:07 +0200

    telescope bulk open in new tab

--- nvim/nvim/after/plugin/telescope.lua
+++ nvim/nvim/after/plugin/telescope.lua
@@ -1,6 +1,24 @@
 local builtin = require('telescope.builtin')
+local action_state = require('telescope.actions.state')
+local actions = require('telescope.actions')
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+  builtin.live_grep({
+    prompt_title = "Grep",
+    attach_mappings = function(_, map)
+      map('i', '<CR>', function(bufnr)
+        local picker = action_state.get_current_picker(bufnr)
+        local multi_selections = picker:get_multi_selection()
+        actions.close(bufnr)
+
+        for _, entry in ipairs(multi_selections) do
+          vim.cmd('tabnew ' .. entry.filename)
+          vim.fn.setpos('.', {0, entry.lnum, entry.col, 0})
+        end
+      end)
+      return true
+    end
+  })
 end)
